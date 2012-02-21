8dd3081fcf3c848a86a69faa6f69e64033a47793
Ajit Singh <ajit.singh@mosaic.com>
2012-02-21 16:06:40 -0500

    added feed method to pull posts on a given page

--- lib/mosaic/facebook/graph/page.rb
+++ lib/mosaic/facebook/graph/page.rb
@@ -3,6 +3,10 @@ module Mosaic
     module Graph
       class Page < Mosaic::Facebook::Graph::GraphObject
         attr_accessor :id, :name, :picture, :link, :likes, :category, :can_post, :type
+
+        def feed
+          @feed ||= AssociationProxy.new(Mosaic::Facebook::Graph::Post, "/#{self.id}/feed")
+        end
       end
     end
   end
