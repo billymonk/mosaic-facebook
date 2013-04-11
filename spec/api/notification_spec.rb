22548af79e6f38df54a92399f71eab8a3b909494
S. Brent Faulkner <brent.faulkner@mosaic.com>
2013-04-11 12:54:40 -0400

    switch to rspec 2.x expect notation

--- spec/api/notification_spec.rb
+++ spec/api/notification_spec.rb
@@ -7,18 +7,18 @@ describe Mosaic::Facebook::Notification do
 
   context "when no or invalid token is passed" do
     it "should require an access_token" do
-      lambda { Mosaic::Facebook::Notification.send_email(options.merge(:access_token => nil)) }.should raise_error(Mosaic::Facebook::AccessTokenError, /Invalid application ID/i)
+      expect { Mosaic::Facebook::Notification.send_email(options.merge(:access_token => nil)) }.to raise_error(Mosaic::Facebook::AccessTokenError, /Invalid application ID/i)
     end
 
     it "should require a valid access_token" do
-      lambda {Mosaic::Facebook::Notification.send_email(options.merge(:recipients => RSpec.configuration.user_id))}.should raise_error(Mosaic::Facebook::AccessTokenError, /INVALID OAUTH ACCESS TOKEN/i)
+      expect { Mosaic::Facebook::Notification.send_email(options.merge(:recipients => RSpec.configuration.user_id)) }.to raise_error(Mosaic::Facebook::AccessTokenError, /INVALID OAUTH ACCESS TOKEN/i)
     end
 
   end
 
   context "when a valid access_token is provided" do
     it "should send a notification" do
-      Mosaic::Facebook::Notification.send_email(options.merge(:recipients => RSpec.configuration.user_id, :access_token => RSpec.configuration.access_token))
+      expect { Mosaic::Facebook::Notification.send_email(options.merge(:recipients => RSpec.configuration.user_id, :access_token => RSpec.configuration.access_token)) }.to_not raise_error
     end
   end
 end
--- spec/graph/page_spec.rb
+++ spec/graph/page_spec.rb
@@ -4,14 +4,14 @@ describe Mosaic::Facebook::Graph::Page do
   context "when a valid token and page id are passed" do
     it "should return a page feed" do
       feed = Mosaic::Facebook::Graph::Page.new().feed.all({ :access_token => RSpec.configuration.access_token, :id => RSpec.configuration.page_id })
-      feed.should be_an_instance_of Array
-      feed.first.should be_an_instance_of Mosaic::Facebook::Graph::Post
+      expect(feed).to be_an_instance_of Array
+      expect(feed.first).to be_an_instance_of Mosaic::Facebook::Graph::Post
     end
 
     it "should return a event list" do
       events = Mosaic::Facebook::Graph::Page.new().events.all({ :access_token => RSpec.configuration.access_token, :id => RSpec.configuration.page_id })
-      events.should be_an_instance_of Array
-      events.first.should be_an_instance_of Mosaic::Facebook::Graph::Event
+      expect(events).to be_an_instance_of Array
+      expect(events.first).to be_an_instance_of Mosaic::Facebook::Graph::Event
     end
   end
 end
