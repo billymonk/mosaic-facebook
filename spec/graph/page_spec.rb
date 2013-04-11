d7a3657c297d6ac48321a3062b46d44afe137fc9
S. Brent Faulkner <brent.faulkner@mosaic.com>
2013-04-11 15:18:30 -0400

    add comment explaining why the limit of 3 is used

--- spec/graph/page_spec.rb
+++ spec/graph/page_spec.rb
@@ -8,6 +8,8 @@ describe Mosaic::Facebook::Graph::Page do
 
     context "with a valid access token" do
       it "should return a page feed" do
+        # FIXME: limit=3 work-around for facebook bug as per:
+        #        https://developers.facebook.com/bugs/525301577527928?browse=search_516709daab5da1453067889
         feed = @page.feed.all(:access_token => RSpec.configuration.access_token, :limit => 3)
         expect(feed).to be_an_instance_of Array
         expect(feed.first).to be_an_instance_of Mosaic::Facebook::Graph::Post
