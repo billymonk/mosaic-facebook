96e190dbc4a5cac4cabb4f096f0ce7f6c39517b2
S. Brent Faulkner <brent.faulkner@mosaic.com>
2013-04-11 14:11:31 -0400

    refactor to use a single page object

--- spec/graph/page_spec.rb
+++ spec/graph/page_spec.rb
@@ -1,17 +1,23 @@
 require File.expand_path('../../spec_helper.rb', __FILE__)
 
 describe Mosaic::Facebook::Graph::Page do
-  context "when a valid token and page id are passed" do
-    it "should return a page feed" do
-      feed = Mosaic::Facebook::Graph::Page.new().feed.all({ :access_token => RSpec.configuration.access_token, :id => RSpec.configuration.page_id })
-      expect(feed).to be_an_instance_of Array
-      expect(feed.first).to be_an_instance_of Mosaic::Facebook::Graph::Post
+  context "given a valid page" do
+    before(:all) do
+      @page = Mosaic::Facebook::Graph::Page.new(:id => RSpec.configuration.page_id)
     end
 
-    it "should return a event list" do
-      events = Mosaic::Facebook::Graph::Page.new().events.all({ :access_token => RSpec.configuration.access_token, :id => RSpec.configuration.page_id })
-      expect(events).to be_an_instance_of Array
-      expect(events.first).to be_an_instance_of Mosaic::Facebook::Graph::Event
+    context "with a valid access token" do
+      it "should return a page feed" do
+        feed = @page.feed.all(:access_token => RSpec.configuration.access_token, :limit => 3)
+        expect(feed).to be_an_instance_of Array
+        expect(feed.first).to be_an_instance_of Mosaic::Facebook::Graph::Post
+      end
+
+      it "should return a event list" do
+        events = @page.events.all(:access_token => RSpec.configuration.access_token)
+        expect(events).to be_an_instance_of Array
+        expect(events.first).to be_an_instance_of Mosaic::Facebook::Graph::Event
+      end
     end
   end
 end
